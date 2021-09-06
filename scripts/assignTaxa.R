ffb3c1626c7eb21c31450f0efa3454ab62666500
johnne <john.sundh@scilifelab.se>
2021-09-06 05:26:53 +0200

    Update input variable names

--- scripts/assignTaxa.R
+++ scripts/assignTaxa.R
@@ -4,17 +4,17 @@ log <- file(snakemake@log[[1]], open="wt")
 sink(log)
 sink(log, type="message")
 
-input <- snakemake@input[[1]]
+seqs <- snakemake@input$seqs
 output <- snakemake@output[[1]]
-refFasta <- snakemake@input[[2]]
+training_fasta <- snakemake@input$training_fasta
 minBoot <- snakemake@params$minboot
 outputBootstraps <- snakemake@params$outputBootstraps
 threads <- snakemake@threads
 
 library(dada2)
 set.seed(100)
-training_fasta <- system.file("extdata", refFasta, package="dada2")
-taxonomy <- assignTaxonomy(input, training_fasta, minBoot=minBoot,
+refFasta <- training_fasta #system.file("extdata", training_fasta, package="dada2")
+taxonomy <- assignTaxonomy(seqs=seqs, refFasta=refFasta, minBoot=minBoot,
                            outputBootstraps=outputBootstraps,
                            multithread=threads, verbose=TRUE)
 rownames(taxonomy) <- names(rownames(taxonomy))
