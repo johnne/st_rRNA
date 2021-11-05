049d6bbb9b3e214f9cfb323cd525c83e651233bc
johnne <john.sundh@scilifelab.se>
2021-11-05 07:31:46 +0100

    Fix syntax

--- scripts/filter_seqs.py
+++ scripts/filter_seqs.py
@@ -20,7 +20,7 @@ def main(sm):
     with openfn(sm.input[0], 'rt') as fhin, openfn(sm.output[0], 'wt') as fhout:
         for record in SeqIO.parse(fhin, sm.params.seq_type):
             if check_seq(record.seq):
-                records.apend(record)
+                records.append(record)
     with openfn(sm.output[0], 'wt') as fhout:
         SeqIO.write(records, fhout, sm.params.seq_type)
 
