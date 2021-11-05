ab19cc6f0b5edb9d0e42a0a0925de9cb5887d7ee
johnne <john.sundh@scilifelab.se>
2021-11-05 22:26:16 +0100

    Fix R script species assignment

--- scripts/assignTaxa.R
+++ scripts/assignTaxa.R
@@ -20,13 +20,20 @@ taxonomy <- assignTaxonomy(seqs=seqs, refFasta=refFasta, minBoot=minBoot,
                            outputBootstraps=outputBootstraps, tryRC=tryRC,
                            multithread=threads, verbose=TRUE)
 df <- as.data.frame(taxonomy)
+# Remove sequences with numbered suffix (resulting from duplicates)
+df <- df[-grep("[0-9]", rownames(df)), ]
+sequences <- as.character(dimnames(taxonomy$tax)[[1]])
+seqids <- names(dimnames(taxonomy$tax)[[1]])
+seqidmap <- as.data.frame(seqids, row.names=sequences)
 # Add species
 df <- addSpecies(df, spFasta)
-rownames(df) <- names(dimnames(taxonomy$tax)[[1]])
+rownames(df) <- seqidmap[rownames(df),]
 taxdf <- df[, grepl("tax.", colnames(df))]
 colnames(taxdf) <- gsub("tax.", "", colnames(taxdf))
+rownames(taxdf) <- gsub(" .+", "", rownames(taxdf))
 bootdf <- df[, grepl("boot", colnames(df))]
 colnames(bootdf) <- gsub("boot.", "", colnames(bootdf))
+rownames(bootdf) <- gsub(" .+", "", rownames(bootdf))
 write.table(taxdf, taxdf_file, sep="\t", quote=FALSE)
 write.table(bootdf, bootdf_file, sep="\t", quote=FALSE)
 print("DONE")
