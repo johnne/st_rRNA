b2a5426d70a8efd4a7331e695e7196e9997bb842
johnne <john.sundh@scilifelab.se>
2021-11-08 09:39:28 +0100

    Add better check for record id

--- scripts/reformat_taxref.py
+++ scripts/reformat_taxref.py
@@ -7,7 +7,7 @@ def main(sm):
     with open(sm.input[0], 'r') as fhin, open(sm.output[0], 'w') as fhout:
         for record in parse(fhin, "fasta"):
             items = (record.id).split(";")
-            if len(items) == 7 and items[0] == items[1]:
+            if len(items) == 7 and items[0].replace("Reversed:","").lstrip("_") == items[1]:
                 recid = ";".join(items[1:])
                 fhout.write(f">{recid}\n{record.seq}\n")
             else:
