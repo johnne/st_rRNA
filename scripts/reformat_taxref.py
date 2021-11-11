a1221a92bcf413dc5f16b4bc1c7694d9cbf7f587
johnne <john.sundh@scilifelab.se>
2021-11-11 06:03:11 +0100

    Check number of ranks when reformatting taxref

--- scripts/reformat_taxref.py
+++ scripts/reformat_taxref.py
@@ -3,15 +3,33 @@
 from Bio.SeqIO import parse
 
 
-def main(sm):
-    with open(sm.input[0], 'r') as fhin, open(sm.output[0], 'w') as fhout:
+def get_len(f_in, f_out, l=8):
+    lengths = {}
+    with open(f_in, 'r') as fhin, open(f_out, 'w') as fhout:
         for record in parse(fhin, "fasta"):
             items = (record.id).split(";")
-            if len(items) == 7 and items[0].replace("Reversed:","").lstrip("_") == items[1]:
-                recid = ";".join(items[1:])
+            try:
+                lengths[len(items)]+=1
+            except KeyError:
+                lengths[len(items)]=1
+            if len(items) == l:
+                fhout.write(f">{record.description}\n{record.seq}\n")
+    return lengths
+
+
+
+
+def main(sm):
+    num_ranks = sm.params.num_ranks
+    with open(sm.input[0], 'r') as fhin, open(sm.output[0], 'w') as fhout:
+        for record in parse(fhin, "fasta"):
+            record.id = (record.id).rstrip(";")
+            items = (record.id).split(";")
+            if items[0] == items[1]:
+                items = items[1:]
+            recid = ";".join(items)
+            if len(items) == num_ranks:
                 fhout.write(f">{recid}\n{record.seq}\n")
-            else:
-                fhout.write(f">{record.id}\n{record.seq}\n")
 
 
 if __name__ == "__main__":
